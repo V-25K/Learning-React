3db60b1aeb41c650fd6e3a7d0c4876ab146672ae
V-25K <vibhorkothari25@gmail.com>
2024-03-31 18:31:54 +0530

    added memoryCard game(not completed)

--- 05-miniGames/src/components/games/MemoryCards.jsx
+++ 05-miniGames/src/components/games/MemoryCards.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React from "react";
+import "../games styling/MemoryCards.css";
 
 function MemoryCards() {
   return (
-    <h1>Memory Game here..</h1>
-  )
+    <div className="game-main-container">
+      <CreateCards />
+    </div>
+  );
 }
 
-export default MemoryCards
+export default MemoryCards;
+
+function CreateCards() {
+  const cardsArray = [
+    "🚗",
+    "🏴‍☠️",
+    "🛺",
+    "🚑",
+    "🚒",
+    "🏍️",
+    "✈️",
+    "🚀",
+    "⛽",
+    "🌋",
+    "🚗",
+    "🏴‍☠️",
+    "🛺",
+    "🚑",
+    "🚒",
+    "🏍️",
+    "✈️",
+    "🚀",
+    "⛽",
+    "🌋",
+  ];
+
+  const shuffledArray = shuffle(cardsArray);
+
+  const cardCreation = shuffledArray.map((card, index) => (
+    <div key={index} className="card-container">
+      <input type="checkbox" id={`card${index}`} />
+      <label htmlFor={`card${index}`} className="card">
+        <div className="card-front">{card}</div>
+        <div className="card-back">
+          <img
+            src="https://w1.pngwing.com/pngs/608/471/png-transparent-games-icon-video-games-game-controllers-symbol-racing-video-game-icon-design-mobile-game-yellow-thumbnail.png"
+            alt="cards back pattern"
+          />
+        </div>
+      </label>
+    </div>
+  ));
+  return cardCreation;
+}
+
+//shuffling the cards in random
+function shuffle(array) {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
